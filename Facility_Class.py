f2e12e10f800f4de3df31f2a0e31b1ac55cbd195
Yannick <y.ciomer@web.de>
2022-11-08 20:44:03 +0100

    adding file

---
+++ Facility_Class.py
@@ -0,0 +1,14 @@
+import random as rd
+import numpy as np
+
+class Facility:
+    pass
+
+class Demand:
+    pass
+
+def Randomize_Demand(set_size: int, dim: int = 2) -> list:
+    pass
+
+if __name__ == "__main__":
+    pass
