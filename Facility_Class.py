808d16609ffbe9f06fe251e752d390c3c04c63f0
Yannick <y.ciomer@web.de>
2022-11-08 22:24:43 +0100

    added Connections option

--- Facility_Class.py
+++ Facility_Class.py
@@ -1,5 +1,4 @@
 import random as rd
-import numpy as np
 import matplotlib.pyplot as plt
 
 """ Classes """
@@ -12,14 +11,21 @@ class Demand:
 class Facility:
     def __init__(self, pos: tuple, demand: Demand) -> None:
         self.position = pos
-        self.service = [demand]
+        self.service = []
+        self.Add_Service(demand)
 
     def Add_Service(self, demand: Demand) -> None:
         self.service.append(demand)
+        demand.facility = self
 
 def Split_Position(points: list):
     x_pos = [point.position[0] for point in points]
     y_pos = [point.position[1] for point in points]
+    return x_pos, y_pos
+
+def Get_Service_Connections(facility: Facility) -> list:
+    x_pos = [(point.position[0], facility.position[0]) for point in facility.service]
+    y_pos = [(point.position[1], facility.position[1]) for point in facility.service]
     return x_pos, y_pos
 
 class Draw:
@@ -31,15 +37,14 @@ class Draw:
     def Plot(self, show_rel: bool = False):
         # Preparing the plot.
         figure, axes = plt.subplots()
-        plt.title(f"Facility Location\nArea:{self.area}\nDemand:{len(self.demands)} - Facilities: {len(self.facilities)}")
+        figure.canvas.set_window_title("Facility Location")
+        figure.set_size_inches(10, 7)
+        plt.title(f"Area:{self.area}\nDemand:{len(self.demands)} - Facilities: {len(self.facilities)}")
         plt.grid(True, which="both")
         axes.set_aspect("equal")
 
-        plt.xlim([0, self.area[0]])
-        plt.ylim([0, self.area[1]])
-
-        plt.axhline(linewidth=1, color="black")
-        plt.axvline(linewidth=1, color="black")
+        plt.xlim([-1, self.area[0] + 1])
+        plt.ylim([-1, self.area[1] + 1])
 
         # get data points to plot.
         x_demand, y_demand = Split_Position(self.demands)
@@ -47,10 +52,14 @@ class Draw:
 
         # demand will be shown as black dots, facilities as red stars
         plt.scatter(x_demand, y_demand , color="black", s=50, zorder=2)
-        plt.scatter(x_facilities, y_facilities , color="red", s=25, marker="*", zorder=2)
+        plt.scatter(x_facilities, y_facilities , color="red", s=50, marker="*", zorder=2)
 
+        # if you want to see which facility serves which demand, set it to True.
         if show_rel:
-            pass
+            for facility in self.facilities:
+                x_service, y_service = Get_Service_Connections(facility)
+                for i in range(0, len(x_service)):
+                    plt.plot(x_service[i], y_service[i], color="grey", zorder=-2)
 
         plt.show()
 
@@ -64,13 +73,12 @@ def Generate_Stream(set_size: int, area: tuple) -> list:
     return [Randomize_Demand(area) for i in range(0, set_size)]
 
 if __name__ == "__main__":
-    test_demand_1 = Demand((3,4))
-    test_demand_2 = Demand((1,3))
+    test_demand = Demand((3,4))
+    test_stream = Generate_Stream(3, (5,5))
 
-    test_facility = Facility((2,3), test_demand_1)
-    test_facility.Add_Service(test_demand_2)
+    test_facility = Facility((2,3), test_demand)
+    for demand in test_stream:
+        test_facility.Add_Service(demand)
 
-    print(len(test_facility.service))
-
-    test_draw = Draw((5,5), [test_demand_1, test_demand_2], [test_facility])
-    test_draw.Plot()
+    test_draw = Draw((5,5), [test_demand, *test_stream], [test_facility])
+    test_draw.Plot(True)
