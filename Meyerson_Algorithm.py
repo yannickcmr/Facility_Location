dfab2807e0b633b18d35bb927ba6af09f5d81334
Yannick <y.ciomer@web.de>
2022-11-09 17:13:02 +0100

    added q_meyerson

--- Meyerson_Algorithm.py
+++ Meyerson_Algorithm.py
@@ -18,10 +18,14 @@ def Find_Nearest_Facility(demand: Demand, facility_list: list):
     return np.min(norm), facility_list[np.argmin(norm)]
 
 # calculates the relative distance based on the cost of opening a new facility.
-def Get_Minimum(distance: float, cost: float) -> float:
+def Get_Probability(distance: float, cost: int) -> float:
     relative_distance = np.around(distance/cost, decimals=3)
     return min(relative_distance, 1)
 
+def q_Get_Probability(q, distance: float, cost: int):
+    relative_distance = np.around(distance/cost, decimals=3)
+    relative_distance = q*relative_distance
+    return min(relative_distance, 1)
 
 def Flip_Coin(prob: float) -> bool:
     return rd.random() < prob
@@ -34,7 +38,25 @@ def Meyerson_Algorithm_Online(demand_list: list, facility_cost: int = 1):
     for demand in demand_list:
         # calculate the relevent values
         norm, next_facility = Find_Nearest_Facility(demand, facilities_list)
-        probability_facility = Get_Minimum(norm, facility_cost)
+        probability_facility = Get_Probability(norm, facility_cost)
+
+        #if next_facility != None: print(f"{probability_facility} -- {norm} -- {demand.position} -- {next_facility.position}")
+        if Flip_Coin(probability_facility):
+            # opens up a new facility.
+            facilities_list.append(Facility(demand.position, demand))
+        else:
+            # uses already existing facility.
+            next_facility.Add_Service(demand)
+    
+    return facilities_list
+
+
+def q_Meyerson_Algorithm_Online(q: float, demand_list: list, facility_cost: int = 1):
+    facilities_list = []
+    for demand in demand_list:
+        # calculate the relevent values
+        norm, next_facility = Find_Nearest_Facility(demand, facilities_list)
+        probability_facility = q_Get_Probability(q, norm, facility_cost)
 
         #if next_facility != None: print(f"{probability_facility} -- {norm} -- {demand.position} -- {next_facility.position}")
         if Flip_Coin(probability_facility):
@@ -51,6 +73,10 @@ if __name__ == "__main__":
     test_area = (100, 20)
     test_stream = Generate_Stream(35, test_area)
 
-    test_meyerson = Meyerson_Algorithm_Online(test_stream, 40)
+    test_meyerson = Meyerson_Algorithm_Online(test_stream, 30)
     result = Draw(test_area, test_stream, test_meyerson)
-    result.Plot(True)
+    #result.Plot(True)
+
+    test_q_meyerson = q_Meyerson_Algorithm_Online(0.5, test_stream, 30)
+    q_result = Draw(test_area, test_stream, test_q_meyerson)
+    #q_result.Plot(True)
